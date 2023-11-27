15a02fc5dab1fb6d9d5fc8bcfa129c2d7da06cbf
SeM2x <houssem135.302@gmail.com>
2023-11-27 16:38:21 +0100

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find("object"): str.find("programming") + 11] + " " + str[str.find("with"): str.find("with") + 4] + " " + str[:6]
+str = str[39:66] + " " + str[str.find("wi"):str.find("wi") + 4] + " " + str[:6]
 print(str)
