c45eaada9f7df95cfc4299e4b7238e29ff12bd2b
SeM2x <houssem135.302@gmail.com>
2023-11-28 14:58:25 +0100

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
     for j in range(10):
         if (j > i):
             if (i != 8):
-                print("{}{}, ".format(i, j), end="")
+                print("{}{}".format(i, j), end=", ")
             else:
                 print("{}{}".format(i, j))
