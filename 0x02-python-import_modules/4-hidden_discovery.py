d6e26fd58c7e83cb49fef24eee1383ec54bc0fbe
SeM2x <houssem135.302@gmail.com>
2023-11-30 22:13:29 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-for v in dir(hidden_4):
-    if (v[0] != '_'):
-        print(v)
+if __name__ == "__main__":
+
+    import hidden_4
+    for v in dir(hidden_4):
+        if (v[0] != '_'):
+            print(v)
