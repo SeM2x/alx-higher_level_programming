eda190cfdc81b9812bdf090394c96a2637554786
SeM2x <houssem135.302@gmail.com>
2023-12-02 18:24:37 +0100

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list), 0, -1):
-        print("{:d}".format(my_list[i - 1]))
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    if not (idx < 0 or idx > (len(new_list) - 1)):
+        new_list[idx] = element
+    return new_list
