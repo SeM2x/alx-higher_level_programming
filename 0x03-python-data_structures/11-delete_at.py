66e87fe03056d6f8f3a05f0f321d9117d4bf2fd2
SeM2x <houssem135.302@gmail.com>
2023-12-03 14:14:51 +0100

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    my_list = [item for i, item in enumerate(my_list) if i != idx]
+    my_list[:] = [item for i, item in enumerate(my_list) if i != idx]
     return my_list
