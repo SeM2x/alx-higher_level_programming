8636d451490bb609cd9357bfbc289e2203fde88b
SeM2x <houssem135.302@gmail.com>
2023-12-03 13:09:09 +0100

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    new = ()
+    max_length = max(len(tuple_a), len(tuple_b))
+    tuple_a += (0,) * (max_length - len(tuple_a))
+    tuple_b += (0,) * (max_length - len(tuple_b))
+    for item1, item2 in zip(tuple_a, tuple_b):
+        new += (item1 + item2,)
+    return new
