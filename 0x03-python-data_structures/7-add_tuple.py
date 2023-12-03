5b12a4751ecafdc784ea97d19a17bad2c2a62ee6
SeM2x <houssem135.302@gmail.com>
2023-12-03 13:25:39 +0100

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,8 +2,12 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     new = ()
     max_length = max(len(tuple_a), len(tuple_b))
+    if max_length == 0:
+        new = (0, 0)
     tuple_a += (0,) * (max_length - len(tuple_a))
     tuple_b += (0,) * (max_length - len(tuple_b))
     for item1, item2 in zip(tuple_a, tuple_b):
         new += (item1 + item2,)
+    if max_length == 1:
+        new += (0,)
     return new
