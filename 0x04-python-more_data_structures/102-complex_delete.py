50ebced7b26509f416ef2ca043bf2c72e438c405
SeM2x <houssem135.302@gmail.com>
2023-12-06 18:41:00 +0100

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keys = []
+    for key, val in a_dictionary.items():
+        if val == value:
+            keys.append(key)
+    for key in keys:
+        del a_dictionary[key]
+
+    return a_dictionary
