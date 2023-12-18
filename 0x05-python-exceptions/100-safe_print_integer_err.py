f0052fc154ec33b6035b3fa5ff466b9980ff368a
SeM2x <houssem135.302@gmail.com>
2023-12-18 13:38:09 +0100

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value), file=sys.stderr)
+    except ValueError as ve:
+        print("Exception: {}".format(ve))
+        return False
+    return True
