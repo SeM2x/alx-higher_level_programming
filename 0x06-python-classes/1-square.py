71cd494730b45c8ae7ed75bca4109961b3df86f0
SeM2x <houssem135.302@gmail.com>
2023-12-19 17:40:27 +0100

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a Sqaure class"""
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a square instance"""
+
+        self.size = size
