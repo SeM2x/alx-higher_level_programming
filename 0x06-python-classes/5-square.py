89450b45f558cb64cfa3a17b1dfff2ee6d39a568
SeM2x <houssem135.302@gmail.com>
2023-12-19 18:26:55 +0100

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Define a Sqaure class"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initialize a square instance"""
+        self.size = size
+
+    @property
+    def size(self):
+        """returns the private attribute size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets size to value"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns the area of the square"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
