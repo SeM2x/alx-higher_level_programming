aae27c8cf82b2f219b3e5d00ae94ff8011d0a9a4
SeM2x <houssem135.302@gmail.com>
2023-12-20 01:17:28 +0100

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,17 +7,6 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a square instance"""
-
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-    def __init__(self, size=0):
-        """Initialize a square instance"""
-
         self.size = size
 
     @property
