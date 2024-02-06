707136abc98f8b219c87ac717b3318f38576d803
SeM2x <houssem135.302@gmail.com>
2024-02-06 11:01:40 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,10 +11,8 @@ def add_integer(a, b=98):
     TypeError: If either of a or b is a non-integer and non-float.
     """
 
-    if (a == float("inf") or a == math.nan or
-            (not isinstance(a, int) and not isinstance(a, float))):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if (b == float("inf") or b == math.nan or
-            (not isinstance(b, int) and not isinstance(b, float))):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
