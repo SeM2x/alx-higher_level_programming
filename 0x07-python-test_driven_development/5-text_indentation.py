0ff49baecc7ba31904166749cbfd8e6842f8415f
SeM2x <houssem135.302@gmail.com>
2024-02-07 18:06:14 +0100

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" function that prints a text with 2 new
+    lines after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """indentate a text
+
+    Args:
+        text: the text to indentate
+    Raises:
+        TypeError: if text is not a sting
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    text = text.replace(".", ".\n\n")
+    text = text.replace("?", "?\n\n")
+    text = text.replace(":", ":\n\n")
+    print(text)
