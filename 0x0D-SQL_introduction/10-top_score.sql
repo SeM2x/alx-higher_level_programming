ee602cf6b6a4a4b5c75054206502c585b6cc5e0b
SeM2x <houssem135.302@gmail.com>
2024-02-13 22:42:34 +0100

    11-best_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- Lists all records of the table second_table of the database hbtn_0c_0 in my MySQL server.
-SELECT score, name from second_table ORDER by score DESC;
+SELECT score, name from second_table ORDER BY score DESC;
