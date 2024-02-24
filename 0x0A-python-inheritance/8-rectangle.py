218e96bbeabf1b277732b81d5ea0fb0c0d49e700
SeM2x <houssem135.302@gmail.com>
2024-02-24 10:33:24 +0100

    8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a rectangle class"""
+
+
+class Rectangle(BaseGeometry):
+    """rectangle class"""
+    __width = 0
+    __height = 0
+
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        __width = width
+        __height = height
