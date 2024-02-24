76ffbfa986e5822c27f88ae4f7b5bdac3912b5da
SeM2x <houssem135.302@gmail.com>
2024-02-24 13:01:13 +0100

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class that inherits from BaseGeometry class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size):
+        """Intialize a new Square.
+
+        Args:
+            size (int): The size of the new Square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Returs a string representation of a Square"""
+        return f"[Square] {self.__width}/{self.__height}"
