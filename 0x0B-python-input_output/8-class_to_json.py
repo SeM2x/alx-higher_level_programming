f509f069c7409cb1496f18e66a183530c7b072ba
SeM2x <houssem135.302@gmail.com>
2024-03-01 17:48:51 +0100

    .\8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines class_to_json function"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+
+    Args:
+        obj (object):  an instance of a Class
+    """
+    return obj.__dict__
