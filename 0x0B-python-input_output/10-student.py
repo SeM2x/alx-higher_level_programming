8f553b29f03bf364402e660368d312de4dcd3b6d
SeM2x <houssem135.302@gmail.com>
2024-03-01 18:27:37 +0100

    .\10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""defines a Student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation method
+
+        Args:
+            first_name (string): first_name
+            last_name (string): last_name
+            age (number): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of
+        a Student instance
+
+        Args:
+            attrs (list): list of attributes to be retrieved
+        Returns:
+            dict: dict representaion of an object
+        """
+        if (attrs):
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+        return self.__dict__
