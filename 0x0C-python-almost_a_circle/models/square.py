80e78a4001055373a7afd93a63501eec2d283f23
SeM2x <houssem135.302@gmail.com>
2024-03-03 13:35:29 +0100

    10. And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a Square class"""
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
