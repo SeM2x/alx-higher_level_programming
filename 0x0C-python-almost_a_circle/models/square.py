31b378023204e9c34b1a3383fc529c8107a88b0d
SeM2x <houssem135.302@gmail.com>
2024-03-03 13:40:24 +0100

    10. And now, the Square!

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,3 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return f'[Square] ({self.id}) {self.x}/{self.y}' \
+            f' - {self.width}'
