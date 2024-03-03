b7ba364a7d9984e8b8310aea81c50472cb982133
SeM2x <houssem135.302@gmail.com>
2024-03-03 11:39:03 +0100

    6. __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -121,3 +121,7 @@ class Rectangle(Base):
             for j in range(self.width):
                 print('#', end='')
             print()
+
+    def __str__(self):
+        return f'[Rectangle] ({self.id}) {self.x}/{self.y}' \
+            f' - {self.width}/{self.height}'
