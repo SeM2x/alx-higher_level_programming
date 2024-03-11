13d9ac2ddc7617391b356a3f506ebcd5d9bb6c9f
SeM2x <houssem135.302@gmail.com>
2024-03-11 12:51:21 +0100

    semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-function factorial(x) {
+function factorial (x) {
   if (!x) {
     return 1;
   }
   return x * factorial(x - 1);
-};
+}
 
 const x = Number(process.argv[2]);
 console.log(factorial(x));
