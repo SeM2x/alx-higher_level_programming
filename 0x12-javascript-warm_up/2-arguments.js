f220d868c21a6f03ff2415ba9059603eea186a1e
SeM2x <houssem135.302@gmail.com>
2024-03-11 11:28:53 +0100

    .\2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 const args = process.argv;
-if (!args[2])
-    console.log('No argument');
-else if (args[2] && !args[3])
-    console.log('Argument found');
-else
-    console.log('Arguments found');
+if (!args[2]) {
+  console.log('No argument');
+}
+else if (args[2] && !args[3]) {
+  console.log('Argument found');
+}
+else {
+  console.log('Arguments found');
+}
