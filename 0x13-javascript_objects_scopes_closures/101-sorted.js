6e5ec6c560f695465c46ff2e762bd8c57995aead
SeM2x <houssem135.302@gmail.com>
2024-03-12 18:45:35 +0100

    .\101-sorted.js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+const newDict = {};
+
+Object.values(dict).forEach((val, index) => {
+  newDict[val] += ' ' + (Object.keys(dict)[index]);
+});
+Object.entries(newDict).forEach(([key, val]) => {
+  newDict[key] = val.replace('undefined ', '').split(' ');
+})
+
+console.log(newDict);
