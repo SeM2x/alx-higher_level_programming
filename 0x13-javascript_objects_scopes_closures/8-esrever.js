02e440554a62529cbd139472c11020950334d280
SeM2x <houssem135.302@gmail.com>
2024-03-12 15:08:39 +0100

    .\8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.reverse();
+  if (list.length <= 1) {
+    return list;
+  }
+  const first = list.shift();
+  const last = list.pop();
+  exports.esrever(list);
+  list.push(first);
+  list.unshift(last);
+  return list;
 };
