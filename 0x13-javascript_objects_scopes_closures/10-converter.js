be8d7970924d4a15681a107894943527942e6df7
SeM2x <houssem135.302@gmail.com>
2024-03-12 17:57:22 +0100

    .\10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-let currentBase = 0;
-
 exports.converter = function (base) {
-  currentBase = base;
-  return decToBase;
-};
-const decToBase = (num) => {
-  return num.toString(currentBase);
-};
+  return (num) => num.toString(base);
+}
