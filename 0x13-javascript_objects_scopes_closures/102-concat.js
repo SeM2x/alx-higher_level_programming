2d09b86e37fd84aef21a9004dcdb4cd642b65a6b
SeM2x <houssem135.302@gmail.com>
2024-03-12 20:08:42 +0100

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -8,12 +8,15 @@ const fileC = process.argv[4];
 let str = '';
 
 fs.readFile(fileA, 'utf8', (err, data) => {
-    if (err) throw err;
-    str += data;
-});
-fs.readFile(fileB, 'utf8', (err, data) => {
-    if (err) throw err;
-    str += data;
-});
+  if (err) throw err;
+  str += data;
 
-fs.writeFile(fileC, str)
+  fs.readFile(fileB, 'utf8', (err, data) => {
+    if (err) throw err;
+    str += data;
+
+    fs.writeFile(fileC, str, err => {
+      if (err) throw err;
+    });
+  });
+});
