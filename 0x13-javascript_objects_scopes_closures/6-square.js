466b7d205f9656dd9c105312d45e3a23ac644681
SeM2x <houssem135.302@gmail.com>
2024-03-12 14:39:41 +0100

    .\6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
-const SquareP = require('./5-square');
+const Rectangle = require('./4-rectangle');
 
-class Square extends SquareP {
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
-      }
-      console.log(s);
+
+    let = 0;
+    while (i < this.height) {
+      console.log(String(char).repeat(this.width));
+      i++;
     }
   }
 }
