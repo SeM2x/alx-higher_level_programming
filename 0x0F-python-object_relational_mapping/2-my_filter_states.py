0909ca03318579b423b132139666db6cec803e27
SeM2x <houssem135.302@gmail.com>
2024-03-16 12:28:40 +0100

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ if (__name__ == '__main__'):
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=USER, passwd=PASS, db=DB)
     cur = db.cursor()
-    cur.execute(f"SELECT * FROM states WHERE name={ARG}\
+    cur.execute(f"SELECT * FROM states WHERE name='{ARG}' \
         ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
