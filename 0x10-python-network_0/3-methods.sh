9274e8600c8570e2be43c4cca6e5a97983cb7f6c
SeM2x <houssem135.302@gmail.com>
2024-03-27 12:14:05 +0100

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept.
-curl -s -X OPTIONS -i 0.0.0.0:5000/route_4 | grep Allow | cut -d" " -f2-
+curl -s -I 0.0.0.0:5000/route_4 | grep Allow | cut -d" " -f2-
