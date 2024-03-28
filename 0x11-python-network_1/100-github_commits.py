fd8483d271b1fd49c8a583730555af3e6fc60d9d
SeM2x <houssem135.302@gmail.com>
2024-03-28 18:01:59 +0100

    .\100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    res = requests.get(
+        url=f"https://api.github.com/repos/{owner}/{repo}/commits", headers=headers)
+
+    commits = sorted(res.json(), key=lambda x: x.get('commit').get('committer').get('date'))[:10]
+    for commit in commits:
+        print(f"{commit.get('sha')}: {commit.get('commit').get('committer').get('name')}")
+        
