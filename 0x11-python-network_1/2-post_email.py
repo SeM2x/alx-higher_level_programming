7a33713c25d9fa7fad04e9fdd342e596d253f2c7
SeM2x <houssem135.302@gmail.com>
2024-03-28 15:08:49 +0100

    .\2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays
+the body of the response (decoded in utf-8)"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({
+        'email': email
+    }).encode('utf-8')
+
+    response = urllib.request.urlopen(url, data=data)
+    print(response.read().decode('utf-8'))
