239ae60ee7707f3d9a0b8ca51332e7c447202108
SeM2x <houssem135.302@gmail.com>
2024-03-28 14:25:17 +0100

    .\0-hbtn_status.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,6 +8,5 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as res:
+    with urllib.request.urlopen(url) as res:
         print(dict(res.headers).get('X-Request-Id'))
