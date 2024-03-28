ba656682060c6947d8da7e6071e79496b654c8dc
SeM2x <houssem135.302@gmail.com>
2024-03-28 16:12:30 +0100

    .\8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     q = ""
     if len(sys.argv) >= 2:
-        q = sys.argv[2]
+        q = sys.argv[1]
 
     res = requests.post(url, data={'q': q})
     try:
