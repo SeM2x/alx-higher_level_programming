a1bdb0fdb43f463804c157fef766e8468824f1f2
SeM2x <houssem135.302@gmail.com>
2024-03-28 18:16:01 +0100

    .\100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -16,8 +16,7 @@ if __name__ == "__main__":
     res = requests.get(
         url=f"https://api.github.com/repos/{owner}/{repo}/commits", headers=headers)
 
-    commits = sorted(res.json(), key=lambda x: x.get(
-        'commit').get('committer').get('date'), reverse=True)[:10]
+    commits = res.json()
     for commit in commits:
         print(f"{commit.get('sha')}: {commit.get(
             'commit').get('author').get('name')}")
