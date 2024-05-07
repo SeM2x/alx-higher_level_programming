58e4af6f1455aed955a6b8330d2f37c3caf8c18e
SeM2x <houssem135.302@gmail.com>
2024-05-07 02:17:32 +0100

    .\4-script.js

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('DIV#toggle_header').click(function (e) { 
+        e.preventDefault();
+        if ($('header').hasClass('red')) {
+            $('header').removeClass('red');
+            $('header').addClass('green');
+        } else {
+            $('header').removeClass('green');
+            $('header').addClass('red');
+        }
+    });
+});
