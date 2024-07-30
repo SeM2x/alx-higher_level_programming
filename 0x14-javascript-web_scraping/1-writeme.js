ced9ca444d9783f730cc3f4bee4367fdf80d5c6b
SeM2x <houssm135.302@gmail.com>
2024-07-30 16:22:42 +0100

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) console.error('Error writing file:', err);
 });
