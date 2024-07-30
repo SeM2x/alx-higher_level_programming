805d6894fe42230495ef34426ca8d11a67a6bddf
SeM2x <houssm135.302@gmail.com>
2024-07-30 17:04:37 +0100

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(response.body).title);
+  }
+});
