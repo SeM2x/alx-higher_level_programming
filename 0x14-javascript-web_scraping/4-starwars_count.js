9d5465ca2a246d1f68a51b15d3964db1c9a5205c
SeM2x <houssm135.302@gmail.com>
2024-07-30 17:45:04 +0100

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,16 +3,20 @@ const request = require('request');
 const api = 'https://swapi-api.alx-tools.com/api';
 const url = process.argv[2];
 request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const films = JSON.parse(response.body).results;
-    const count = films?.reduce((count, film) => {
-      if (film.characters?.indexOf(`${api}/people/18/`) !== -1) {
-        return count + 1;
+  if (!error) {
+    if (response.statusCode === 200) {
+      const films = JSON.parse(response.body).results;
+      const count = films?.reduce((count, film) => {
+        if (film.characters?.indexOf(`${api}/people/18/`) !== -1) {
+          return count + 1;
+        }
+        return count;
+      }, 0);
+      if (count !== undefined) {
+        console.log(count);
+      } else {
+        console.log(0);
       }
-      return count;
-    }, 0);
-    if (count !== undefined) { console.log(count); } else { console.log(0); }
+    }
   }
 });
