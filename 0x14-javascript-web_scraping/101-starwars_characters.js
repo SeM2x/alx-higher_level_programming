81d61a8cda2fc759626c00f108ebdfc787b80a57
SeM2x <houssm135.302@gmail.com>
2024-07-31 16:17:04 +0100

    101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+request(url, (error, response, body) => {
+  if (!error) {
+    const characters = JSON.parse(response.body).characters;
+    const res = [];
+    characters.forEach(character => {
+      request(character, (error, response, body) => {
+        if (!error) {
+          const result = JSON.parse(response.body);
+          const index = result.url.substring(0, result.url.length - 1).substring(result.url.length - 3);
+          res.push({
+            name: result.name,
+            index: index.startsWith('/') ? index.substring(1) : index
+          });
+          if (characters.length === res.length) {
+            res.sort((a, b) => Number(a.index) - Number(b.index));
+            res.forEach(char => console.log(char.name));
+          }
+        }
+      });
+    });
+  }
+});
