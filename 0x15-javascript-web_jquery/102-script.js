c8b3dcb58454ac6d21cd67a3cc662ed9f7681b0d
SeM2x <houssem135.302@gmail.com>
2024-08-08 01:39:06 +0100

    102-script.js

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    const url = 'https://www.fourtonfish.com/hellosalut/hello/?lang=';
+    $('INPUT#btn_translate').click(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: url + $('INPUT#language_code').val(),
+            method: 'GET',
+            success: function (data) {
+                $('DIV#hello').text(data.hello);
+            }
+        });
+    });
+});
